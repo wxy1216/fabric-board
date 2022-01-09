e870c8d70c8a2fb959560f853e74057808d2bc2f
x24 <wangxy1216@yeah.net>
2022-01-10 00:15:35 +0800

    config

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <a-button>aaa</a-button>
+  </div>
+</template>
+
+<script>
+import { fabric } from "fabric";
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { Button } from 'ant-design-vue';
 
 Vue.config.productionTip = false
+Vue.use(Button);
 
 /* eslint-disable no-new */
 new Vue({
