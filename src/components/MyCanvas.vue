9e9283e67f453b512223c3ccdf42b4ad34161d79
x24 <wangxy1216@yeah.net>
2022-02-26 22:53:33 +0800

    feat: circle & rect & save

--- src/components/MyCanvas.vue
+++ src/components/MyCanvas.vue
@@ -27,6 +27,7 @@ export default {
             mouseFrom: { x: "", y: "" },
             drawingObject: null, // 当前绘制对象
             drawType: "",
+            textbox: null,
         };
     },
     methods: {
@@ -181,29 +182,92 @@ export default {
                 case "erase":
                     break;
                 case "arrow":
-                    canvasObject = new fabric.Path(this.drawArrow(this.mouseFrom.x, this.mouseFrom.y,this.mouseTo.x, this.mouseTo.y, 30, 30), {
-                        fill: "rgba(255,255,255,0)",
+                    canvasObject = new fabric.Path(
+                        this.drawArrow(
+                            this.mouseFrom.x,
+                            this.mouseFrom.y,
+                            this.mouseTo.x,
+                            this.mouseTo.y,
+                            30,
+                            30
+                        ),
+                        {
+                            fill: "rgba(255,255,255,0)",
+                            stroke: this.color,
+                            strokeWidth: this.drawWidth,
+                            evented: false,
+                        }
+                    );
+                    break;
+                case "rectangle":
+                    canvasObject = new fabric.Rect({
+                        top: this.mouseFrom.y,
+                        left: this.mouseFrom.x,
+                        width: this.mouseTo.x - this.mouseFrom.x,
+                        height: this.mouseTo.y - this.mouseFrom.y,
                         stroke: this.color,
                         strokeWidth: this.drawWidth,
-                        evented: false,
+                        // opacity: 0.6,
+                        fill: false,
                     });
                     break;
-
+                case "circle":
+                    canvasObject = new fabric.Circle({
+                        top: this.mouseFrom.y,
+                        left: this.mouseFrom.x,
+                        originX: "center",
+                        originY: "center",
+                        radius: Math.sqrt(
+                            Math.pow(this.mouseTo.x - this.mouseFrom.x, 2) +
+                                Math.pow(this.mouseTo.y - this.mouseFrom.y, 2)
+                        ),
+                        stroke: this.color,
+                        strokeWidth: this.drawWidth,
+                        fill: null,
+                    });
+                    break;
+                case "text":
+                    let textbox = new fabric.Textbox("", {
+                        left: this.mouseFrom.x - 60,
+                        top: this.mouseFrom.y - 20,
+                        width: 150,
+                        fontSize: 18,
+                        borderColor: "#2c2c2c",
+                        fill: this.color,
+                        hasControls: false,
+                    });
+                    this.canvas.add(textbox);
+                    textbox.enterEditing();
+                    textbox.hiddenTextarea.focus();
+                    this.textbox = textbox;
+                    break;
             }
             if (canvasObject) {
                 this.canvas.add(canvasObject);
                 this.drawingObject = canvasObject;
             }
         },
+        download(url) {
+            if (!url) {
+                return;
+            }
+            let a = document.createElement("a");
+            a.setAttribute("href", url);
+            a.download = "未命名.png";
+            a.click();
+            a.remove();
+        },
         startDraw(type) {
+            this.endDraw();
             this.drawType = type;
-            // 关闭画布放大缩小
-            this.closeMouseEvent();
 
             if (this.drawType === "freeDraw") {
                 this.freeDraw();
             } else if (this.drawType === "erase") {
                 this.freeEarse();
+            } else if (this.drawType === "save") {
+                let url = this.canvas.toDataURL();
+                this.download(url);
             } else {
                 // 开启画布绘制监听
                 this.drawListener();
@@ -211,6 +275,10 @@ export default {
         },
         endDraw() {
             // 关闭绘制
+            if (this.textbox) {
+                this.textbox.exitEditing();
+                this.textbox = null;
+            }
             this.closeMouseEvent();
             // 开启放大缩小
             this.initZoom();
@@ -280,15 +348,15 @@ export default {
             theta = typeof theta != "undefined" ? theta : 30;
             headlen = typeof theta != "undefined" ? headlen : 10;
             // 计算各角度和对应的P2,P3坐标
-            var angle = Math.atan2(fromY - toY, fromX - toX) * 180 / Math.PI,
-            angle1 = (angle + theta) * Math.PI / 180,
-            angle2 = (angle - theta) * Math.PI / 180,
-            topX = headlen * Math.cos(angle1),
-            topY = headlen * Math.sin(angle1),
-            botX = headlen * Math.cos(angle2),
-            botY = headlen * Math.sin(angle2);
+            var angle = (Math.atan2(fromY - toY, fromX - toX) * 180) / Math.PI,
+                angle1 = ((angle + theta) * Math.PI) / 180,
+                angle2 = ((angle - theta) * Math.PI) / 180,
+                topX = headlen * Math.cos(angle1),
+                topY = headlen * Math.sin(angle1),
+                botX = headlen * Math.cos(angle2),
+                botY = headlen * Math.sin(angle2);
             var arrowX = fromX - topX,
-            arrowY = fromY - topY;
+                arrowY = fromY - topY;
             var path = " M " + fromX + " " + fromY;
             path += " L " + toX + " " + toY;
             arrowX = toX + topX;
@@ -299,7 +367,7 @@ export default {
             arrowY = toY + botY;
             path += " L " + arrowX + " " + arrowY;
             return path;
-        }
+        },
         // transformMouse(mouseX, mouseY) {
         //     debugger;
         // },
